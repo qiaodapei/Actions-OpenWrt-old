7fdaaa4cd0241086150167da9c700b83414838ce
qiaodapei <qiaodapei@hotmail.com>
2025-01-20 00:32:01 +0800

    first commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd openwrt
+
+# Add luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
+rm -rf theme-temp/luci-theme-opentomcat/LICENSE
+rm -rf theme-temp/luci-theme-opentomcat/README.md
+mv -f theme-temp/luci-theme-opentomcat package/lean/
+rm -rf theme-temp
+# default_theme='opentomcat'
+# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
+# Add luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
+mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
+rm -rf package-temp
